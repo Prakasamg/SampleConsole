46a0ec71029eaa01f0fe0cd6db0281d0088d71f7
Prakasam <prakasamg@outlook.com>
2017-06-13 12:56:04 +0530

    firstpush

---
+++ gitSampleConsole/gitSampleConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gitSampleConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" first compile of console");
+        }
+    }
+}
